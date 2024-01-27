d51c9de6df0b046ea8e3b17fd0fddbb087bf3584
grabm <grabm@SIGMA>
2024-01-27 12:33:40 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SUSPanstwaMiasta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SUSPanstwaMiasta
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
